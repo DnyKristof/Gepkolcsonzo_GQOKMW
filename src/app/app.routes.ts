f70a93e10fcb2aaaa01c9c10263a28622df03825
Kristof <danyi.kristof1998@gmail.com>
2024-04-20 14:20:33 +0200

    Adding routes and creating express project

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,14 @@
 import { Routes } from '@angular/router';
+import { MachinesComponent } from './components/machines/machines.component';
+import { CompaniesComponent } from './components/companies/companies.component';
+import { RentalsComponent } from './components/rentals/rentals.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    { path: 'machines', component: MachinesComponent },
+    { path: 'companies', component: CompaniesComponent },
+    { path: 'rentals', component: RentalsComponent },
+    { path: 'machines/:id', component: MachinesComponent },
+    { path: 'companies/:id', component: CompaniesComponent },
+    { path: 'rentals/:id', component: RentalsComponent }
+
+];
